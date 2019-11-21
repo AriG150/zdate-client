013d082b47764a215b77a731d04f22425291b423
Ari <gryphon150@gmail.com>
2019-11-21 13:52:48 -0800

    started Link.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { BrowserRouter as Router,
         Route, 
         Link } from 'react-router-dom';
@@ -8,13 +8,17 @@ import List from './List';
 function App() {
   // const [ourUser, setourUser] = useState([])
   const [users, setUsers] = useState([])
+  const [userId, setuserId] = useState()
+
   return (
     <div>
       <h2>ZDate</h2>
-      <h5>Are you a short-range user looking for a long-term relationship?</h5>
+      <h5>So...the apocolypse happened. That sucked, right? </h5>
+      <h6>But, like, honestly, in between killing people and running away I still want....companionship...</h6>
+      <h6>Don't you?...</h6>
       <Router>
-      <Link to="/list"> Login </Link>
-        <Route exact path="/list" render={ () => <List users={users} /> } />  
+      <Link to="/list"> Enter </Link>
+        <Route exact path="/list" render={ () => <List users={users} selectUser={setuserId}/> } />  
       </Router>
     </div>
   )
--- src/List.jsx
+++ src/List.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function List(props){
+  const [ourUser, setourUser] = useState('Michonne', 'www.photo.com')
+
+  const mappedUsers = props.users.map((user, id) => (
+    //TODO: change inforation that will show up to include all parts of User schema. 
+    <div onClick={() => props.selectUser(user.id)} key={id} > {user.name}  </div>
+  ))
+
+  // TODO: ourUser information shows up in own div. 
+
+  // mappedListitems = this.state.listItems.map((listItem, id) => <div className="describe" key={id}>
+  //   <Checkbox className="checkbox" checked={listItem.checked} value={listItem._id} label='' onChange={(e) => this.handleChecked(e,id)} />
+  //   <Link className="mapped-items" to={`/profile/${listItem._id}/adventure/${this.props.match.params.cName}`}>{listItem.name}</Link>  
+
+  // useEffect(() => {
+  //   axios.get('http://localhost:3001')
+  //     .then((res) => {
+  //       setUsers(res.data)
+  //     })
+  // }, [])
 
   return(
     <div>
-      <span>Hello. List page </span>
+      <p>{ourUser}</p>
+      <p>Test User 1</p>
+        {mappedUsers}
+
     </div>
   )
 }
