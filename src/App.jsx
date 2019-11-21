d102db6f14b6a9f0e6c98888da1c210d4c34e1f8
Ari <gryphon150@gmail.com>
2019-11-21 14:43:27 -0800

    got names of users to show up on List.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,37 @@ import { BrowserRouter as Router,
         Route, 
         Link } from 'react-router-dom';
 import './App.css';
+import axios from 'axios';
 import Homepage from './Homepage';
 import List from './List';
 import Profile from './Profile'
 
 function App() {
-  // const [ourUser, setourUser] = useState([])
+  const [ourUser, setourUser] = useState([])
   const [users, setUsers] = useState([])
   const [userId, setuserId] = useState()
+
+  //Grab all users 
+  useEffect(() => {
+    axios.get('http://localhost:3001/users/')
+      .then((res) => {
+        setUsers(res.data)
+      })
+  }, [])
+
+  //Person you're signed in as
+  useEffect(()=> {
+    axios.get('https://localhost:3001/users/ouruser')
+      .then((res) => {
+        setourUser(res.data)
+      })
+  }, [])
 
   return (
     <div>
       <Router>
         <Route exact path="/" render={ () => <Homepage  /> } />
-        <Route exact path="/list" render={ () => <List users={users} selectUser={setuserId}/> } /> 
+        <Route exact path="/list" render={ () => <List users={users} ourUser={ourUser} selectUser={setuserId} /> } /> 
         <Route exact path="/profile" render={ () => <Profile users={users} /> } /> 
       </Router>
     </div>
--- src/List.jsx
+++ src/List.jsx
@@ -2,10 +2,9 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 function List(props){
-  const [ourUser, setourUser] = useState('Michonne', 'www.photo.com')
+  // const [ourUser, setourUser] = useState()
 
   const mappedUsers = props.users.map((user, id) => (
-    //TODO: change inforation that will show up to include all parts of User schema. 
     <div onClick={() => props.selectUser(user.id)} key={id} > {user.name}  </div>
   ))
 
@@ -15,16 +14,11 @@ function List(props){
   //   <Checkbox className="checkbox" checked={listItem.checked} value={listItem._id} label='' onChange={(e) => this.handleChecked(e,id)} />
   //   <Link className="mapped-items" to={`/profile/${listItem._id}/adventure/${this.props.match.params.cName}`}>{listItem.name}</Link>  
 
-  // useEffect(() => {
-  //   axios.get('http://localhost:3001')
-  //     .then((res) => {
-  //       setUsers(res.data)
-  //     })
-  // }, [])
 
   return(
     <div>
-      <p>{ourUser}</p>
+      <h2>Humans In Your Area: </h2>
+      <p>{props.ourUser}</p>
       <p>Test User 1</p>
         {mappedUsers}
 
