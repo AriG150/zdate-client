f5f1ca1224ed1ed2ecda6058aaea9f9aca620341
Stanley Staton <stanleystaton@Stanleys-MacBook-Pro-2.local>
2019-11-21 18:42:38 -0800

    i dont know wht to tell you. i guess i got the skills to show

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Skill from './Skill';
 
 function Profile(props) {
 
   let content
+  let skills
+  let photo 
 
   if (!props.selectedUser) {
     // there is no user yet
@@ -12,14 +15,37 @@ function Profile(props) {
     content = <p>My name is {props.selectedUser.name}</p>
   }
   
+  if (!props.selectedUser) {
+    // there is no user yet
+    skills = <p>No skills</p>
+  } else {
+    // we have a user!
+    console.log(props.selectedUser.skills)
+    skills = props.selectedUser.skills.map((skill, idx) => {
+      console.log(skill)
+      // return <li>{skill}</li>
+      return <Skill skill={skill} key={idx} />
+    })
+  }
+
+  if (!props.selectedUser) {
+    // there is no user yet
+    photo = <p>no photo</p>
+  } else {
+    // we have a user!
+    photo = <img src={props.selectedUser.photo} />
+  }
 
   return(
     <div>
       <div>
-        Profile Photo
+        {photo}
       </div>
       <div>
         {content}
+        <ul>
+          {skills}
+        </ul>
       </div>
     </div>
 
---
+++ src/Skill.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const Skill = props => {
+    console.log(props)
+    return (
+        <li>{props.skill}</li>
+    )
+}
+
+export default Skill
