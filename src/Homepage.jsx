55a3cd6ca9a7f6ed94adf00cdd954c70388f5e19
Ari <gryphon150@gmail.com>
2019-11-21 18:42:18 -0800

    started css

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './css/Homepage.css';
 
 function Homepage(){
 
   return(
-    <div>
+    <div className="homepage">
       <h2>ZDate</h2>
       <h5>So...the apocolypse happened. That sucked, right? </h5>
       <h6>But, like, honestly, in between killing people and running away I still want....companionship...</h6>
